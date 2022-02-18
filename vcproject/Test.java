1940a63b8501a29e93d1253c24f7c4f69386befc
Niko Suoniemi <niko.suoniemi@tuni.fi>
2022-02-18 11:04:58 +0200

    vcproject/Test.java: Add function doSomething

--- vcproject/Test.java
+++ vcproject/Test.java
@@ -5,8 +5,14 @@ function printOut(String print){
 	system.out.println(print);
 };
 
+
 	public static void main(String[] args) {
         System.out.println( args[0] );
         System.out.println( args[1] );
     }
+
+function doSomething(String something){
+    system.out.println("Do " + something);
+}
+
 }
